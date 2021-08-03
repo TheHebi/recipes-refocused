c4e6298e1914b9dddb33f38b25318d13da8784b1
TheHebi <nturcotte8@live.com>
2021-08-03 14:04:26 -0700

    routes 90% finished

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,17 +1,110 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const db = require('../../models');
+const db = require("../../models");
+const bcrypt = require(`bcrypt`);
 
-router.get("/",(req,res)=>{
-})
-router.post("/",(req,res)=>{
-})
-router.post("/login",(req,res)=>{
-})
-router.get("/:id",(req,res)=>{
-})
-router.delete("/:id",(req,res)=>{
-})
+// find all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await db.User.findAll({
+      attributes: { exclude: [`createdAt`, `updatedAt`] },
+    });
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// find one user by id tag and associated recipes and comments
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await db.User.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: db.Recipe,
+          attributes: { exclude: [`createdAt`, `updatedAt`] },
+        },
+        {
+          model: db.Comment,
+          attributes: { exclude: [`createdAt`, `updatedAt`] },
+        },
+      ],
+      attributes: { exclude: [`createdAt`, `updatedAt`] },
+    });
+    res
+      .status(200)
+      .json({ message: user ? res.json(user) : `User not found.` });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// create a new user
+router.post("/", (req, res) => {
+  db.User.create(req.body)
+    .then((newUser) => {
+      req.session.user = {
+        id: newUser.id,
+        username: newUser.username,
+        email: newUser.email,
+      };
+      res.json(newUser);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// user login
+router.post("/login", (req, res) => {
+  db.User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((user) => {
+    if (!user) {
+      res.status(403).json({
+        message: "incorrect username or password",
+      });
+    } else {
+      const isPasswordCorrect = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+      if (isPasswordCorrect) {
+        req.session.user = {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          loggedIn: true,
+        };
+        res.json(user);
+      } else {
+        res.status(403).json({
+          message: "incorrect username or password",
+        });
+      }
+    }
+  });
+});
+
+// delete a user by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const delUser = await db.User.destroy({
+      where: { id: req.params.id },
+    });
+    console.log(delUser);
+    res.status(200).json({
+      message: delUser ? `User deleted!` : `User not found.`,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
--- models/Comment.js
+++ models/Comment.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const Recipe = require("./Recipe")
 const User = require("./User")
+
 class Comment extends Model {}
 
 Comment.init(
