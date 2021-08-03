8c3fbf073b84bfd8e6bf001d5a09d8aceef14af3
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-08-03 11:26:24 -0700

    recipe model done

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,7 +1,64 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const User = require('./User')
+const Genre = require('./Genre')
 
 class Recipe extends Model {}
 
 
-module.exports = Recipe
+Recipe.init({
+    recipe_image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            isUrl: true
+        }
+    },
+    recipe_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: (1,50)
+        }
+    },
+    recipe_ingredients: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    recipe_author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'username'
+        }
+    },
+    prep_time: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    cook_time: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    recipe_howto: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    genre_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Genre,
+            key: 'id'
+        }
+    }
+})
+
+module.exports = Recipe;
