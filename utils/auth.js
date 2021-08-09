934cf2e26ae3862021ae43361045dd06d995c12a
Koppi Kolyvek <kk674@cornell.edu>
2021-08-09 13:38:29 -0700

    fixed bug to access create / dashboard while logged in

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const auth = (req, res, next) => {
     // will require users to be logged in, if not logged in will redirect.
-    if (!req.session.logged_in) {
+    if (!req.session.loggedIn) {
       res.redirect('/login');
     } else {
       next();
